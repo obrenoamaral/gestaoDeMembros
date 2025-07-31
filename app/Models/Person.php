b3d5f5bff2a171b5aeab080b73d004cf9a2d3d07
Breno Amaral <bramaral@girolando.com.br>
2025-07-31 17:05:55 -0300

    make models

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,9 +2,43 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'name',
+        'phone',
+        'address',
+        'neighborhood',
+        'city',
+        'state',
+        'zipcode',
+        'country',
+        'company',
+        'website',
+        'description',
+        'image_path',
+        'cover_path',
+        'category_id',
+        'subcategory_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
 }
